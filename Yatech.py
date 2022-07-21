4b34520c34cf9c904a28abf55d2dc7864bf7b7d9
Juarez Ribeiro de Freitas Junior <juarezribeiro94@gmail.com>
2022-07-21 15:02:16 -0300

    update

--- Yatech.py
+++ Yatech.py
@@ -1,13 +1,35 @@
 import pytesseract as tess
 tess.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 from PIL import Image
+import os
+import pandas as pd
 
-imagem_teste = Image.open('pag2.PNG')
+pasta = r'C:\Python Scripts\banco_de_imagens'
 
-def extrair_texto():
-    # Extrai texto da imagem
-    texto = tess.image_to_string(imagem_teste, lang="por")
-    print(texto)
+# Cria lista de imagens e questões em texto
+banco_imagens = []
+questoes_str = []
 
 
-extrair_texto()
+def listar_imagens():
+    # Lista as imagens
+    for nome in os.listdir(pasta):
+        nome_ = "\\" + nome
+        arquivo = pasta + nome_
+        arquivo.replace(' ', '')
+        banco_imagens.append(arquivo)
+
+
+def extrair_texto_lista():
+    # Extrai texto das imagens listadas
+    for imagens in banco_imagens:
+        imag = Image.open(imagens)
+        texto = tess.image_to_string(imag, lang="por")
+        questoes_str.append(texto)
+
+
+listar_imagens()
+extrair_texto_lista()
+
+df = pd.DataFrame(questoes_str)
+df.to_excel(r'C:\Python Scripts\venv\teste.xlsx')
